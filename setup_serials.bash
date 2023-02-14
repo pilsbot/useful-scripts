1920c0d65d57fbf93bdb476de02a5805ece8f6f7
Pascal <accounts@pascalpieper.de>
2023-02-14 22:12:09 +0100

    Added udev-rules for ttyACM devices

--- setup_serials.bash
+++ setup_serials.bash
@@ -5,12 +5,13 @@ sudo systemctl stop serial-getty@*
 
 pushd /dev
 
-sudo ln -s ttyTHS1 ttyJ41_EXP_HEADER
-sudo ln -s ttyS0   ttyJ50
+sudo ln -s ttyTHS1 ttyJ41_EXP_HEADER || true
+sudo ln -s ttyS0   ttyJ50 || true
 
-sudo ln -s ttyJ50  ttyHoverboard
-sudo ln -s ttyACM0 ttyHeadMCU
+sudo ln -s ttyJ50  ttyHoverboard || true
 
-sudo ln -s ttyACM1 ttyGPS
+#sudo ln -s ttyACM0 ttyHeadMCU
+sudo ln -s ttySparkfunProMicro* ttyHeadMCU || true
+sudo ln -s ttyGPS* ttyGPS || true
 
 popd
