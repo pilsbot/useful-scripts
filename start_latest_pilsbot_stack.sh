7e22bd52b0ce9257c2482e69cf1d8101b52cf9b1
Pascal <accounts@pascalpieper.de>
2023-02-15 21:22:28 +0100

    Now pulling newest docker image if internet connection is detected

--- start_latest_pilsbot_stack.sh
+++ start_latest_pilsbot_stack.sh
@@ -2,4 +2,13 @@
 
 #todo: make sure docker binds to correct interwebs interface
 echo "For joystick teleop, type 'ros2 launch pilsbot_driver pilsbot_teleop.launch.py'"
-docker run -it --privileged --net=host --volume /dev:/dev pilsbot/pilsbot_hw_ros:latest
+
+if ping -c 2 pilsbot.de &> /dev/null
+then
+  echo "You seem to have a working internet connection, lets pull the newest docker image from the interwebs"
+  docker pull pilsbot/pilsbot_hw_ros:latest
+fi
+
+
+# see pilsbot_vscode_ws/.devcontainer/devcontainer.json
+docker run -it --privileged --network=host --cap-add=SYS_PTRACE --security-opt=seccomp:unconfined --security-opt=apparmor:unconfined --volume=/tmp/.X11-unix:/tmp/.X11-unix  --volume /dev:/dev pilsbot/pilsbot_hw_ros:latest
