5b0e19f4b8e258fa98402432ddd1c6dbda8759d9
Pascal <accounts@pascalpieper.de>
2023-11-19 15:42:15 +0100

    start, jetzt mit 30% mehr rosbag

--- start_latest_pilsbot_stack.sh
+++ start_latest_pilsbot_stack.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #todo: make sure docker binds to correct interwebs interface
-echo "For joystick teleop, type 'ros2 launch pilsbot_driver pilsbot_teleop.launch.py'"
 
 if ping -c 2 pilsbot.de &> /dev/null
 then
@@ -9,6 +8,6 @@ then
   docker pull pilsbot/pilsbot_hw_ros:latest
 fi
 
-
+echo "For joystick teleop, type 'ros2 launch pilsbot_driver pilsbot_teleop.launch.py'"
 # see pilsbot_vscode_ws/.devcontainer/devcontainer.json
-docker run -it --privileged --network=host --cap-add=SYS_PTRACE --security-opt=seccomp:unconfined --security-opt=apparmor:unconfined --volume=/tmp/.X11-unix:/tmp/.X11-unix  --volume /dev:/dev pilsbot/pilsbot_hw_ros:latest
+docker run -it --privileged --gpus=all --volume=/media/sda/bags:/workspaces/bags --volume=/tmp/argus_socket:/tmp/argus_socket --network=host --cap-add=SYS_PTRACE --security-opt=seccomp:unconfined --security-opt=apparmor:unconfined --volume=/tmp/.X11-unix:/tmp/.X11-unix  --volume /dev:/dev pilsbot/pilsbot_hw_ros:latest
