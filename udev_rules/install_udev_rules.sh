e23e11d5e214dd572c20df3484513139ebf65f8d
Pascal <accounts@pascalpieper.de>
2023-12-30 00:00:56 +0100

    Merged udev roulezz

--- udev_rules/install_udev_rules.sh
+++ udev_rules/install_udev_rules.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-sudo cp ./50-ds4drv.rules /etc/udev/rules.d/
+sudo cp ./*.rules /etc/udev/rules.d/
 echo "Reloading udev rules and trigger new rules"
 sudo udevadm control --reload-rules
 sudo udevadm trigger
