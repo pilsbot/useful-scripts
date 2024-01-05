0d234daaa53642d4233341cc6ce98bf6e8fadbca
Pascal <accounts@pascalpieper.de>
2024-01-05 19:26:32 +0100

    more help

--- setup_serials.bash
+++ setup_serials.bash
@@ -15,4 +15,11 @@ sudo ln -s ttyJ50  ttyHoverboard || true
 sudo ln -s ttySparkfunProMicro* ttyHeadMCU || true
 sudo ln -s ttyGPS* ttyGPS || true
 
+
+echo "The following files shoud exist and point to something that exists:"
+file ttySparkfunProMicro*
+file ttyGPS*
+file ttyHeadMCU*
+file ttyLighting*
+
 popd
