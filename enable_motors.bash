89df093efbf7ed6791708e1dc476b106b2e5a8f7
Pascal <accounts@pascalpieper.de>
2024-01-12 00:20:55 +0100

    lets be witty about that

--- enable_motors.bash
+++ enable_motors.bash
@@ -4,4 +4,4 @@ set -e
 i2cset -y -r 1 0x20 0x00 0x00
 i2cset -y -r 1 0x20 0x14 0xff
 
-echo "TODO: Emulating \"ON\" button press is not implemented yet. Please press it yourself on the hoverboard plate."
+echo "Emulating \"ON\" button press is actually implemented. ROS2 'head_mcu' will do that."
