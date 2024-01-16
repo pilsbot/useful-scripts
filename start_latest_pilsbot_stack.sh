fdb8062d4abc12096296f46fd7021d7f6e943e2c
Pascal <accounts@pascalpieper.de>
2024-01-16 23:17:56 +0100

    more precise docker start sequence as in pilsbot_vscode_ws

--- start_latest_pilsbot_stack.sh
+++ start_latest_pilsbot_stack.sh
@@ -8,6 +8,19 @@ then
   docker pull pilsbot/pilsbot_hw_ros:latest
 fi
 
-echo "For joystick teleop, type 'ros2 launch pilsbot_driver pilsbot_teleop.launch.py'"
+echo "For joystick teleop, type 'ros2 launch pilsbot_bringup pilsbot.launch.py'"
 # see pilsbot_vscode_ws/.devcontainer/devcontainer.json
-docker run -it --privileged --gpus=all --volume=/media/sda/bags:/workspaces/bags --volume=/tmp/argus_socket:/tmp/argus_socket --network=host --cap-add=SYS_PTRACE --security-opt=seccomp:unconfined --security-opt=apparmor:unconfined --volume=/tmp/.X11-unix:/tmp/.X11-unix  --volume /dev:/dev pilsbot/pilsbot_hw_ros:latest
+docker run -it \
+--privileged \
+--user ros \
+--gpus=all \
+--volume=/tmp/argus_socket:/tmp/argus_socket \
+--network=host --cap-add=SYS_PTRACE \
+--security-opt=seccomp:unconfined \
+--security-opt=apparmor:unconfined \
+--volume=/tmp/.X11-unix:/tmp/.X11-unix \
+--volume /dev:/dev \
+--volume /home/pilsbot/pilsbot_vscode_ws:/workspaces/pilsbot_vscode_ws \
+pilsbot/pilsbot_hw_ros:latest
+
+#--volume=/media/externSSD/bags:/workspaces/bags \
